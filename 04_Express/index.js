3e47b124783e3cd76e44bfd848a69705d943a491
emmanuelbaiao <emmanuelbaiao@gmail.com>
2022-11-09 20:57:06 -0300

    configurado o express

--- 04_Express/index.js
+++ 04_Express/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const path     = require('path')
 const express  = require('express');
 const app      = express();
@@ -6,6 +7,14 @@ const basePath = path.join(__dirname, 'templates');
 
 app.get('/produtos', (requisicao, resposta)=>{
     resposta.sendFile("``")
+=======
+const express = require('express');
+const app     = express();
+const porta   = 3000; //porta para acesso ao servidor
+
+app.get('/produtos', (requisicao, resposta)=>{
+    resposta.send("Esta é minha primeira página utilizando o NODE.js")
+>>>>>>> 196819b (configurado o express)
 })
 
 app.listen(porta, ()=> {
