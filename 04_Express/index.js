196819b759cb14d28db08324439d15c14ad53d5b
emmanuelbaiao <emmanuelbaiao@gmail.com>
2022-11-09 20:57:06 -0300

    configurado o express

--- 04_Express/index.js
+++ 04_Express/index.js
@@ -1,2 +1,12 @@
 const express = require('express');
-const 
+const app     = express();
+const porta   = 3000; //porta para acesso ao servidor
+
+app.get('/produtos', (requisicao, resposta)=>{
+    resposta.send("Esta é minha primeira página utilizando o NODE.js")
+})
+
+app.listen(porta, ()=> {
+    console.log("A aplicação está rodando na porta "+porta);
+})
+
