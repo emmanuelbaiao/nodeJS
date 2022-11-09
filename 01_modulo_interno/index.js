830e71e90f03152126f3db630609053cb96cbb21
emmanuelbaiao <emmanuelbaiao@gmail.com>
2022-11-08 21:01:43 -0300

    incluído primeiro modulo

---
+++ 01_modulo_interno/index.js
@@ -0,0 +1,14 @@
+const meuModulo = require('./meu_modulo');
+const soma = meuModulo.soma;
+const divisao = meuModulo.divisao;
+const subtracao = meuModulo.subtracao;
+const multiplicacao = meuModulo.multiplicacao;
+
+
+console.log(soma(10,5));
+
+console.log(divisao(10,5));
+
+console.log(subtracao(10,5));
+
+console.log(multiplicacao(10,5));
